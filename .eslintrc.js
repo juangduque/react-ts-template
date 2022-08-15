720d977fe51fd4c72ef0c45f9b959038b496d47a
Juan Duque <juangduque@gmail.com>
2022-08-15 10:46:58 -0500

    chore: add type check extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
 	extends: [
 		'airbnb',
 		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:import/typescript',
 		'prettier',
