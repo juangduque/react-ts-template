15ebccb8c6f3811ea2708a2f860764e7befc6b03
Juan Duque <juan.guillermo.duque@jeronimo-martins.com>
2024-04-17 14:02:51 -0500

    feat: add router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './styles/global.scss';
-import App from './App';
+import { App } from './router';
 
 
 const root = ReactDOM.createRoot(
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,8 @@
+import '../../styles/app.scss';
+
+
+export const Home = () => (
+	<main>
+		<h1>Hello world!</h1>
+	</main>
+);
